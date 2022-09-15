b3faa7dd1b97009b1330ac763d45667afafde93e
Pietro Cairo <92369535+pieCairo@users.noreply.github.com>
2022-09-15 09:42:31 +0200

    Fine lezione 15/09

---
+++ Laboratorio/EserciziLaboratorio/Es3-StringaPalindromaPuntatori/cmake-build-debug/CMakeFiles/Es3_StringaPalindromaPuntatori.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Es3_StringaPalindromaPuntatori.dir/main.c.obj"
+  "Es3_StringaPalindromaPuntatori.pdb"
+  "Es3_StringaPalindromaPuntatori.exe"
+  "Es3_StringaPalindromaPuntatori.exe.manifest"
+  "Es3_StringaPalindromaPuntatori.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Es3_StringaPalindromaPuntatori.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Laboratorio/EserciziLaboratorio/Es3-StringaPalindromaPuntatori/cmake-build-debug/CMakeFiles/Es3_StringaPalindromaPuntatori.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Es3_StringaPalindromaPuntatori.
+# This may be replaced when dependencies are built.
